e76230738ed7c9de62ebd088681359d5d1ef3535
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-30 17:34:45 +0700

    feat: Linked list

---
+++ src/BasicAlgorithms/DataStructure/LinkedList.js
@@ -0,0 +1,96 @@
+/* eslint-disable max-classes-per-file */
+class Node {
+	constructor(value) {
+		this.value = value
+
+		this.next = null
+	}
+}
+
+class LinkedList {
+	constructor() {
+		this.head = null
+	}
+
+	push(value) {
+		const node = new Node(value)
+
+		if (!this.head) {
+			this.head = node
+
+			return
+		}
+
+		let currentNode = this.head
+
+		while (currentNode.next) {
+			currentNode = currentNode.next
+		}
+
+		currentNode.next = node
+	}
+
+	print() {
+		if (!this.head) return null
+
+		let currentNode = this.head
+
+		while (currentNode) {
+			console.log(currentNode.value)
+
+			currentNode = currentNode.next
+		}
+	}
+
+	remove(value) {
+		if (!this.head) return
+
+		let currentNode = this.head
+
+		while (currentNode) {
+			if (currentNode.next.value === value) {
+				currentNode.next = currentNode.next.next
+
+				break
+			}
+
+			currentNode = currentNode.next
+		}
+	}
+
+	pop() {
+		let currentNode = this.head
+
+		while (currentNode.next.next) {
+			currentNode = currentNode.next
+		}
+
+		currentNode.next = null
+	}
+
+	removeIndex(index) {
+		let currentNode = this.head
+
+		for (let i = 1; i < index; i++) {
+			currentNode = currentNode.next
+		}
+
+		currentNode.next = currentNode.next.next
+	}
+}
+
+const linkedList = new LinkedList()
+
+linkedList.push('a')
+linkedList.push('b')
+linkedList.push('c')
+
+linkedList.print()
+
+console.log('-------------------')
+
+// linkedList.remove('b')
+// linkedList.pop()
+linkedList.removeIndex(1)
+
+linkedList.print()
