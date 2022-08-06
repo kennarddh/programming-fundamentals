e71d01e5d7b1d8cd6d98f1b626707ef08df60df3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-06 21:35:03 +0700

    test: Nested describe

--- src/BasicAlgorithms/DataStructure/LinkedList/__tests__/Single.js
+++ src/BasicAlgorithms/DataStructure/LinkedList/__tests__/Single.js
@@ -7,200 +7,212 @@ describe('Single linked list', () => {
 		linkedList = new SingleLinkedList()
 	})
 
-	it('Should print', () => {
-		expect.assertions(2)
+	describe('Print', () => {
+		it('Should print', () => {
+			expect.assertions(2)
 
-		const log = jest.spyOn(console, 'log').mockImplementation(() => {})
+			const log = jest.spyOn(console, 'log').mockImplementation(() => {})
 
-		const result = linkedList.print()
+			const result = linkedList.print()
 
-		linkedList.push('foo')
+			linkedList.push('foo')
 
-		linkedList.print()
+			linkedList.print()
 
-		expect(result).toBeNull()
-		expect(log.mock.calls[0][0]).toBe('foo')
+			expect(result).toBeNull()
+			expect(log.mock.calls[0][0]).toBe('foo')
+		})
 	})
 
-	it('Should push', () => {
-		expect.assertions(4)
+	describe('Push', () => {
+		it('Should push', () => {
+			expect.assertions(4)
 
-		linkedList.push('foo')
-		linkedList.push('bar')
-		linkedList.push('foobar')
+			linkedList.push('foo')
+			linkedList.push('bar')
+			linkedList.push('foobar')
 
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next.value).toBe('bar')
-		expect(linkedList.head.next.next.value).toBe('foobar')
-		expect(linkedList.head.next.next.next).toBeNull()
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('bar')
+			expect(linkedList.head.next.next.value).toBe('foobar')
+			expect(linkedList.head.next.next.next).toBeNull()
+		})
 	})
 
-	it('Should remove', () => {
-		expect.assertions(3)
+	describe('Remove', () => {
+		it('Should remove', () => {
+			expect.assertions(3)
 
-		linkedList.push('foo')
-		linkedList.push('bar')
-		linkedList.push('foobar')
+			linkedList.push('foo')
+			linkedList.push('bar')
+			linkedList.push('foobar')
 
-		linkedList.remove('bar')
+			linkedList.remove('bar')
 
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next.value).toBe('foobar')
-		expect(linkedList.head.next.next).toBeNull()
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('foobar')
+			expect(linkedList.head.next.next).toBeNull()
+		})
+
+		it('Should not remove value if linked list is empty', () => {
+			expect.assertions(2)
+
+			const result = linkedList.remove('foo')
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBeNull()
+		})
+
+		it('Should set head to null if linked list has 1 node and remove value', () => {
+			expect.assertions(2)
+
+			linkedList.push('foo')
+
+			const result = linkedList.remove('foo')
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBe('foo')
+		})
+
+		it('Should not remove value if value not exist', () => {
+			expect.assertions(3)
+
+			linkedList.push('foo')
+
+			const result = linkedList.remove('bar')
+
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next).toBeNull()
+
+			expect(result).toBeNull()
+		})
 	})
 
-	it('Should pop', () => {
-		expect.assertions(4)
+	describe('Pop', () => {
+		it('Should pop', () => {
+			expect.assertions(4)
 
-		linkedList.push('foo')
-		linkedList.push('bar')
-		linkedList.push('foobar')
+			linkedList.push('foo')
+			linkedList.push('bar')
+			linkedList.push('foobar')
 
-		const result = linkedList.pop()
+			const result = linkedList.pop()
 
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next.value).toBe('bar')
-		expect(linkedList.head.next.next).toBeNull()
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('bar')
+			expect(linkedList.head.next.next).toBeNull()
 
-		expect(result).toBe('foobar')
+			expect(result).toBe('foobar')
+		})
+
+		it('Should not pop if linked list is empty', () => {
+			expect.assertions(2)
+
+			const result = linkedList.pop()
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBeNull()
+		})
+
+		it('Should set head to null if linked list has 1 node and pop', () => {
+			expect.assertions(2)
+
+			linkedList.push('foo')
+
+			const result = linkedList.pop()
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBe('foo')
+		})
 	})
 
-	it('Should remove index', () => {
-		expect.assertions(4)
+	describe('Remove index', () => {
+		it('Should remove index', () => {
+			expect.assertions(4)
 
-		linkedList.push('foo')
-		linkedList.push('bar')
-		linkedList.push('foobar')
+			linkedList.push('foo')
+			linkedList.push('bar')
+			linkedList.push('foobar')
 
-		const result = linkedList.removeIndex(1)
+			const result = linkedList.removeIndex(1)
 
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next.value).toBe('foobar')
-		expect(linkedList.head.next.next).toBeNull()
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('foobar')
+			expect(linkedList.head.next.next).toBeNull()
 
-		expect(result).toBe('bar')
+			expect(result).toBe('bar')
+		})
+
+		it('Should not remove index if linked list is empty', () => {
+			expect.assertions(2)
+
+			const result = linkedList.removeIndex(0)
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBeNull()
+		})
+
+		it('Should set head to null if linked list has 1 node and remove index', () => {
+			expect.assertions(2)
+
+			linkedList.push('foo')
+
+			const result = linkedList.removeIndex(0)
+
+			expect(linkedList.head).toBeNull()
+			expect(result).toBe('foo')
+		})
+
+		it('Should not remove index if index not exist', () => {
+			expect.assertions(3)
+
+			linkedList.push('foo')
+
+			const result = linkedList.removeIndex(1)
+
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next).toBeNull()
+
+			expect(result).toBeNull()
+		})
 	})
 
-	it('Should not pop if linked list is empty', () => {
-		expect.assertions(2)
+	describe('Index of', () => {
+		it('Should return index indexOf', () => {
+			expect.assertions(3)
 
-		const result = linkedList.pop()
+			linkedList.push('foo')
 
-		expect(linkedList.head).toBeNull()
-		expect(result).toBeNull()
-	})
+			const result = linkedList.indexOf('foo')
 
-	it('Should set head to null if linked list has 1 node and pop', () => {
-		expect.assertions(2)
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next).toBeNull()
 
-		linkedList.push('foo')
+			expect(result).toBe(0)
+		})
 
-		const result = linkedList.pop()
+		it('Should return index indexOf if value is not first', () => {
+			expect.assertions(4)
 
-		expect(linkedList.head).toBeNull()
-		expect(result).toBe('foo')
-	})
+			linkedList.push('foo')
+			linkedList.push('bar')
 
-	it('Should not remove value if linked list is empty', () => {
-		expect.assertions(2)
+			const result = linkedList.indexOf('bar')
 
-		const result = linkedList.remove('foo')
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('bar')
+			expect(linkedList.head.next.next).toBeNull()
 
-		expect(linkedList.head).toBeNull()
-		expect(result).toBeNull()
-	})
+			expect(result).toBe(1)
+		})
 
-	it('Should set head to null if linked list has 1 node and remove value', () => {
-		expect.assertions(2)
+		it('Should return -1 indexOf if linked list empty', () => {
+			expect.assertions(2)
 
-		linkedList.push('foo')
+			const result = linkedList.indexOf('foo')
 
-		const result = linkedList.remove('foo')
-
-		expect(linkedList.head).toBeNull()
-		expect(result).toBe('foo')
-	})
-
-	it('Should not remove value if value not exist', () => {
-		expect.assertions(3)
-
-		linkedList.push('foo')
-
-		const result = linkedList.remove('bar')
-
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next).toBeNull()
-
-		expect(result).toBeNull()
-	})
-
-	it('Should not remove index if linked list is empty', () => {
-		expect.assertions(2)
-
-		const result = linkedList.removeIndex(0)
-
-		expect(linkedList.head).toBeNull()
-		expect(result).toBeNull()
-	})
-
-	it('Should set head to null if linked list has 1 node and remove index', () => {
-		expect.assertions(2)
-
-		linkedList.push('foo')
-
-		const result = linkedList.removeIndex(0)
-
-		expect(linkedList.head).toBeNull()
-		expect(result).toBe('foo')
-	})
-
-	it('Should not remove index if index not exist', () => {
-		expect.assertions(3)
-
-		linkedList.push('foo')
-
-		const result = linkedList.removeIndex(1)
-
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next).toBeNull()
-
-		expect(result).toBeNull()
-	})
-
-	it('Should return index indexOf', () => {
-		expect.assertions(3)
-
-		linkedList.push('foo')
-
-		const result = linkedList.indexOf('foo')
-
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next).toBeNull()
-
-		expect(result).toBe(0)
-	})
-
-	it('Should return index indexOf if value is not first', () => {
-		expect.assertions(4)
-
-		linkedList.push('foo')
-		linkedList.push('bar')
-
-		const result = linkedList.indexOf('bar')
-
-		expect(linkedList.head.value).toBe('foo')
-		expect(linkedList.head.next.value).toBe('bar')
-		expect(linkedList.head.next.next).toBeNull()
-
-		expect(result).toBe(1)
-	})
-
-	it('Should return -1 indexOf if linked list empty', () => {
-		expect.assertions(2)
-
-		const result = linkedList.indexOf('foo')
-
-		expect(linkedList.head).toBeNull()
-		expect(result).toBe(1)
+			expect(linkedList.head).toBeNull()
+			expect(result).toBe(1)
+		})
 	})
 })
