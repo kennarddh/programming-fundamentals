64050fee809f1bd32e37950f4a3551c646ce17d3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-10 15:01:33 +0700

    test: Add new test remove index single linked list

--- src/BasicAlgorithms/DataStructure/LinkedList/__tests__/Single.test.js
+++ src/BasicAlgorithms/DataStructure/LinkedList/__tests__/Single.test.js
@@ -306,6 +306,24 @@ describe('Single linked list', () => {
 
 			expect(result).toBeNull()
 		})
+
+		it('Should set next to null', () => {
+			expect.assertions(5)
+
+			linkedList.push('foo')
+			linkedList.push('foobar')
+			linkedList.push('bar')
+
+			const result = linkedList.removeIndex(2)
+
+			expect(linkedList.head.value).toBe('foo')
+			expect(linkedList.head.next.value).toBe('foobar')
+			expect(linkedList.head.next.next).toBeNull()
+
+			expect(linkedList.size).toBe(2)
+
+			expect(result).toBe('bar')
+		})
 	})
 
 	describe('Index of', () => {
